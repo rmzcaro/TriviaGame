d9226f352091fdfbca5b7468ffc4d9dfade6937e
Carolina RC <caro.rmz315@gmail.com>
2018-02-28 06:38:35 -0600

    added pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+/*  Pseudocode 
+Create timer: start and what to do when time is up
+
+User input for each question which calculates wrong, right
+Doesn't allow for more than one input 
+
+A counter keeps track of answers
+
+Create background and questions 
+*/ 
