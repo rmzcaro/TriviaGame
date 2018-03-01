47c92091cd1c736c6c7ef66114b16b87343332b1
Carolina RC <caro.rmz315@gmail.com>
2018-02-28 19:12:14 -0600

    printed question to console

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -39,12 +39,42 @@ var score = 0;
 // To hold the index of a current question 
 var questionIndex = 0; 
 //Array of questions 
-var questionsArray = [questions.q1];
+var questionsArray = [questions.q1, questions.q2];
 
 //Functions ==================================================================
 // Function to render questions
 function renderQuestion() {
-
+if (questionIndex <= (questionsArray.length - 1)) {
+    document.querySelector("#question").innerHTML = questionsArray[questionIndex][0];
+}
 
 }
 
+//PROCESS 
+
+//Calling function to start the game
+renderQuestion();
+
+document.onkeyup = function(event) {
+
+    //if there are not more questions, stop the function 
+    if (questionIndex === questionsArray.length) {
+        return;
+    }
+
+    /*Identify which key was pressed, make it lowercase, set it to user input
+    variable */
+    var userInput = String.fromCharCode(event.which).toLowerCase();
+
+    //Only run this code if "t" or "f" were pressed 
+    if (userInput ==="t" || userInput ==="f") {
+    //if correct answer, increase and update score
+    if(userInput === questionsArray[questionIndex][1]){
+    score++;
+    updateScore();
+
+    //Incremenet the questionIndex variable and call the renderQuestion function
+    questionIndex++;
+    renderQuestion();
+
+    }}
