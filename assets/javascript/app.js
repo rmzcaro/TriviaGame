c76228b34e80c9f5f798dde0c2ea65e3d654a57e
Carolina RC <caro.rmz315@gmail.com>
2018-03-02 00:17:58 -0600

    added timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,6 +26,63 @@ A counter keeps track of answers
 Create background and questions 
 */ 
 
+//CODE BEGINS HERE
+
+window.onload = function() {
+    $("#stop").on("click", stopwatch.stop);
+    $("#reset").on("click", stopwatch.reset);
+    $("#start").on("click", stopwatch.start);
+  };
+
+
+  //Var that will hold interval that runs stopwatch
+  var intervalId;
+
+// Prevents clock from speeding up unnecessarily 
+var clockRunning = false;
+
+//Stopwatch Object
+var stopwatch = {
+    time: 0,
+
+reset: function(){
+
+    stopwatch.time = 0;
+
+    //Display div to 00
+    $("display").text("00");
+},
+
+start: function() {
+    // use setInterval to start the count here and set clock to running
+    if(!clockRunning){
+        intervalId = setInterval(stopwatch.count, 10);
+        clockRunning = true;
+    }
+},
+stop: function() {
+    //use clearInterval to  stop the count here 
+    clearInterval(intervalId);
+    clockRunning = false;
+},
+count: function(){
+    //Increment time 
+    stopwatch.time++;
+
+    //Get the current time
+    var converted = stopwatch.timeConverter(stopwatch.time);
+    console.log(converted);
+
+    //Use variable created to show the converted tim in display
+    $("display").text(converted);
+},
+timeConverter: function(t){
+    var minutes = Math.floor(t/60);
+    var seconds = t - (minutes * 60);
+}
+
+
+}
 // Variables ================================================================
 
 //Object question for quiz game//
@@ -50,10 +107,17 @@ if (questionIndex <= (questionsArray.length - 1)) {
 
 }
 
+//Function that updates the score
+function updateScore(){
+    document.querySelector("#score").innerHTML = "Score: " + score;
+
+}
+
 //PROCESS 
 
 //Calling function to start the game
 renderQuestion();
+updateScore();
 
 document.onkeyup = function(event) {
 
@@ -73,8 +137,12 @@ document.onkeyup = function(event) {
     score++;
     updateScore();
 
+    }
+
     //Incremenet the questionIndex variable and call the renderQuestion function
     questionIndex++;
     renderQuestion();
 
-    }}
+    }
+
+};
