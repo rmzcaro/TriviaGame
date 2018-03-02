ab2cac01f73e2c149b2077e3dff92c60a5380873
Carolina RC <caro.rmz315@gmail.com>
2018-03-02 00:22:35 -0600

    finishing timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -79,7 +79,21 @@ count: function(){
 timeConverter: function(t){
     var minutes = Math.floor(t/60);
     var seconds = t - (minutes * 60);
+
+    if (seconds < 10){
+        seconds = "0" + seconds;
+    }
+
+    if (minutes ===0){
+        minutes ="00";
+    }
+
+    else if (minutes < 10){
+        minutes = "0" + minutes;
+    }
+    return "" + seconds;
 }
+
 
 
 }
