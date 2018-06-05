3e8fa875671b3c90f74bd53a280bdd77dc347239
Carolina RC <caro.rmz315@gmail.com>
2018-06-04 23:26:31 -0500

    added click event listener

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,48 @@
+// TRIVIA BEGINS HERE 
+
+// object answers
+var answers = {
+    q1: ["2"],
+    q2: ["4"]
+};
+
+// score of 0 
+var score = 0; 
+
+// var to hold the index of current answer
+var answersIndex = 0; 
+
+// array of answers 
+var answersArray = [answers.q1, answers.q2];
+
+// function that updates the score
+function updateScore() {
+    $("#score").append("score: line8 " + score);
+}
+
+// MAIN TRIVIA BEGINS HERE
+
+// when a user presses a key, it will run the function 
+$("#triviaGame").click(function() {
+    console.log("clicked");
+
+})
+// document.onkeyup = function(event) {
+
+    // // determine which key was pressed
+    // var userInput = String.fromCharCode(event.which); 
+
+    // // if correct answer, increase and update score
+    // if (userInput === answersArray[answersIndex][0]) {
+    //     console.log("clicked");
+
+    // }
+
+// }
+// -------------------------------------------------
+
+// TIMER BEGINS HERE 
+
 // upon load show that 5 seconds left 
 window.onload = function () {
     $("#start").on("click", countdown.start)
