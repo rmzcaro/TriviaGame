cf6a502b4dbdc2bfcc967854da183c416de112a0
Carolina RC <caro.rmz315@gmail.com>
2018-06-20 20:02:17 -0500

    adding function to render questions one by one instead

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,95 @@
-// TRIVIA BEGINS HERE 
+// VARIABLES 
+
+// questions begin here 
+var questions = {
+    q1: ["Black Panther has a degree in: "],
+    q2: ["What rare metal is mined in Wakanda?"]
+};
 
 // object answers
 var answers = {
     q1: ["2"],
-    q2: ["4"]
+    q2: ["4"],
+    q3: ["1"],
 };
 
-// score of 0 
-var score = 0; 
+// answers correct
+var score = 0;
+
+// wrong answers
+var wrong = 0;
+
+// unanswered answers
+var unanswered = 0;
+
+// hold questions index
+var questionIndex = 0;
+
+questionsArray = [questions.q1, questions.q2];
 
 // var to hold the index of current answer
-var answersIndex = 0; 
+var answersIndex = 0;
 
 // array of answers 
 var answersArray = [answers.q1, answers.q2];
 
-// function that updates the score
-function updateScore() {
-    $("#score").append("score: line8 " + score);
-}
+// =======================================================
 
 // MAIN TRIVIA BEGINS HERE
 
+// if more questions render next question
+function showQuestion() {
+    if (questionIndex <= (questionsArray.length - 1))
+
+    {
+        $("#triviaGame").html(questionsArray[questionIndex][0]);
+    }
+    // if there aren't render the end of the game 
+    else {
+        gameOver();
+    }
+}
+
+showQuestion();
+
+// editing above 
+// ===================================================
+
 // when a user presses a key, it will run the function 
-$("#triviaGame").click(function(event) {
+$("#triviaForm").click(function () {
     // determine which key was pressed
-    var userInput = $("input[type=radio]:checked").val(); 
+    var userInput = $("input[name=question1]:checked").val();
     console.log(userInput);
 
-    // if correct answer, increase and update score
-    if (userInput === answersArray[answersIndex][0]) {
-        console.log("clicks match!");
-
+    // if no answer increase variable of no answers 
+    if (!userInput) {
+        // console.log(userInput);
+        unanswered++;
+        // if correct answer, increase and update score
+    } else if (userInput === answersArray[answersIndex][0]) {
+        console.log("correct!");
+        score++;
+        // updateScore();else {
     } else {
-        console.log("no match!")
-    }
+        // if anything else, this means it's incorrect and increate that variable 
+        wrong++;
+        console.log("incorrect!")
+    };
 
-})
 
-    
+    // END TEST
+    // increment the answerIndex variable 
+    answersIndex++;
+});
+
+// POTENTIALLY REMOVE
+// function that showes the correct answers total score
+// function updateScore() {
+//     $("#score").append(score);
+// }
+
+// updateScore();
+
 
 // -------------------------------------------------
 
@@ -46,8 +97,9 @@ $("#triviaGame").click(function(event) {
 
 // upon load show that 5 seconds left 
 window.onload = function () {
-    $("#start").on("click", countdown.start)
+    $("#start").on("click", countdown.start);
 };
+
 
 // interval that holds countdown
 var intervalId;
@@ -55,18 +107,18 @@ var intervalId;
 // prevents clock from speeding up unncecessarily 
 var clockRunning = false;
 
-//counter object 
 var countdown = {
 
     time: 0,
 
     // if start is clicked start counter    
     start: function () {
-        console.log(start + "line 20")
+
         // use the setInterval to start the counter 
+
         if (!clockRunning) {
             intervalId = setInterval(countdown.count, 1000);
-            console.log(intervalId + "line 22")
+            // console.log(intervalId + "line 22")
             clockRunning = true;
         }
     },
@@ -75,18 +127,19 @@ var countdown = {
         countdown.time++;
         // get the current time
         var converted = countdown.timeConverter(countdown.time);
-        console.log(converted);
+        // console.log(converted);
 
         // if 10 seconds went by END THE GAME
-        if (converted === "03") {
+        if (converted === "05") {
             clearInterval(intervalId);
             clockRunning = false;
         }
 
         // use the variable to show the converted time in the display div 
         $("#time-left").text(converted);
-        if (converted === "03") {
-            $("#time-left").text("TIME IS UP");
+        if (converted === "05") {
+            var timeUp = $("#time-left").text("TIME IS UP");
+            gameOver();
         }
 
     },
@@ -99,3 +152,14 @@ var countdown = {
         return seconds;
     }
 };
+
+
+// function when game over
+function gameOver() {
+    $("#score").append("Total correct: " + score);
+    $("#wrong").append("Total incorrect: " + wrong);
+    // total unanswered
+    $("#unanswered").append("Total unanswered: " + unanswered);
+
+
+}
